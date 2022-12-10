7e203a88fbed0bf385575ff73fd81d1d2d44c154
iliyan borisov <iliyan.borisov@ft.com>
2022-12-10 21:20:52 +0200

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { Admin, Resource, ListGuesser } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
-import authProvider from "./authProvider";
-import {EditExams, ListExams} from "./exam";
+import React from 'react'
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-
-const App = () => <Admin authProvider={authProvider} dataProvider={dataProvider} requireAuth>
-    <Resource name="exams" list={ListExams} edit={EditExams} />
-</Admin>;
+const App = () => <div>test</div>
 
 export default App;
