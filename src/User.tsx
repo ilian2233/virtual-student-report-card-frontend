cd25c9b1cb2de2202ae2e9e6d096669e73f6e41a
iliyan borisov <iliyan.borisov@ft.com>
2022-12-18 19:13:33 +0200

    Updated all styles and centered all elements

--- src/User.tsx
+++ src/User.tsx
@@ -1,18 +1,18 @@
 import {useCookies} from "react-cookie";
 import React from "react";
 import {saveUser} from "./axiosRequests";
-import {Button, Grid, TextField} from "@mui/material";
+import {Button, Paper, TextField, Typography} from "@mui/material";
 import {useSnackbar} from "notistack";
 import {requestResult} from "./main";
 
-export type roles = "student"|"teacher"|"admin"
+export type roles = "student" | "teacher" | "admin"
 export const nameRegex = /^([a-zA-Z]\s?)*$/
 export const emailRegex = /^(\S+@\S+\.\S+)?$/
 export const phoneRegex = /^(\d{10})?$/
 
-export const CreateUser = (props:{ role: roles}) => {
+export const CreateUser = (props: { role: roles }) => {
     const [cookies] = useCookies();
-    const { enqueueSnackbar } = useSnackbar();
+    const {enqueueSnackbar} = useSnackbar();
 
     const [name, setName] = React.useState<string>("");
     const [email, setEmail] = React.useState<string>("");
@@ -23,42 +23,51 @@ export const CreateUser = (props:{ role: roles}) => {
         saveUser(name, email, phone, cookies["token"], props.role, requestResult(enqueueSnackbar))
     }
 
-    return <Grid container alignItems="center" direction="column">
-                <Grid item>
-                    <TextField
-                        error={!nameRegex.test(name)}
-                        name="Name"
-                        type="name"
-                        placeholder="Ivan"
-                        label="Name"
-                        value={name}
-                        onChange={ (e) => setName(e.target.value)}
-                    />
-                </Grid>
-                <Grid item>
-                    <TextField
-                        error={!emailRegex.test(email)}
-                        name="Email"
-                        type="email"
-                        placeholder="johndoe@email.com"
-                        label="Email"
-                        value={email}
-                        onChange={ (e) => setEmail(e.target.value)}
-                    />
-                </Grid>
-                <Grid item>
-                    <TextField
-                        error={!phoneRegex.test(phone)}
-                        name="Phone"
-                        type="phone"
-                        placeholder="0881234567"
-                        label="Phone"
-                        value={phone}
-                        onChange={ (e) => setPhone(e.target.value)}
-                    />
-                </Grid>
-                <Button onClick={handleSubmit}>
-                    Submit
-                </Button>
-            </Grid>
+    return <Paper
+        sx={{
+            width: 300,
+            mx: 'auto', // margin left & right
+            my: 4, // margin top & botom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            borderRadius: 'sm',
+            boxShadow: 'md',
+        }}
+        variant="outlined"
+    >
+        <Typography component="h1"><b>Create new {props.role}:</b></Typography>
+        <TextField
+            error={!nameRegex.test(name)}
+            name="Name"
+            type="name"
+            placeholder="Ivan"
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+            error={!emailRegex.test(email)}
+            name="Email"
+            type="email"
+            placeholder="johndoe@email.com"
+            label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+            error={!phoneRegex.test(phone)}
+            name="Phone"
+            type="phone"
+            placeholder="0881234567"
+            label="Phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+        />
+        <Button onClick={handleSubmit}>
+            Submit
+        </Button>
+    </Paper>
 }
